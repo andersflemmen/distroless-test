7f7498509d3b4479e619c7bdbb1d49e071a01ada
Anders Flemmen <anders.flemmen@bouvet.no>
2020-02-04 13:57:26 +0100

    Using debug base image. More debug output.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,19 @@
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Application {
     public static void main(String[] args) throws IOException {
+        System.out.println("Whoami? " + IOUtils.toString(Runtime.getRuntime().exec(new String[]{"sh","-c","whoami"}).getInputStream(), Charset.defaultCharset()));
+        System.out.println("Id: " + IOUtils.toString(Runtime.getRuntime().exec(new String[]{"sh","-c","id"}).getInputStream(), Charset.defaultCharset()));
+        System.out.println("ls -ld /etc/ssl/certs/java/:");
+        System.out.println(IOUtils.toString(Runtime.getRuntime().exec(new String[]{"sh","-c","ls -ld /etc/ssl/certs/java/"}).getInputStream(), Charset.defaultCharset()));
         System.out.println("Verifying certificates...");
         Path path = Path.of(URI.create("file:///etc/ssl/certs/java/"));
         if (Files.exists(path)) {
